c25fef33c3dbdbe30313e5dae860141c197a9a00
Ryan Plitt <ryan.plitt@icloud.com>
2023-10-11 05:54:25 -0600

    Initial Commit

---
+++ FavoriteFoods/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FavoriteFoods
+//
+//  Created by Ryan Plitt on 10/11/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
